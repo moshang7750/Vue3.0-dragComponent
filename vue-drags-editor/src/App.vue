c5781cc3f777a00b1a9cb5665b30dadd0b5a09c2
xbn <1347750780@qq.com>
2021-01-23 10:13:55 +0800

    fist commit

---
+++ vue-drags-editor/src/App.vue
@@ -0,0 +1,17 @@
+<template>hello world</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {}
+})
+</script>
+
+<style lang="scss">
+html,
+body {
+  margin: 0;
+}
+</style>
